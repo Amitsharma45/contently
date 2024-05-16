ba78929e0c1d0da49c793a566f6b09ff1a863bd0
akanchha25 <akanchhakumari11@gmail.com>
2024-05-16 12:53:12 +0530

    subject

--- src/app/(home)/subject/page.js
+++ src/app/(home)/subject/page.js
@@ -1,7 +1,8 @@
+import Subject from '@/components/main/subject'
 import React from 'react'
 
 export default function SubjectPage() {
   return (
-    <div>page</div>
+    <Subject />
   )
 }
---
+++ src/components/main/subject/index.js
@@ -0,0 +1,136 @@
+import React from "react";
+
+function Subject() {
+  const data = [
+    {
+      title: "Management",
+      logo: "logo",
+      about:
+        "Management (or managing) is the administration of organizations, whether they are a business, a nonprofit organization, or a government body through business administration, nonprofit management, or the political science sub-field of public administration respectively. It is the process of managing the resources of businesses, governments, and other organizations.",
+
+      service: "Services Offered",
+      points: [
+        "Management assignment help",
+        "Doing a Management assignment",
+        "Offering pre-made Management assignments for sale",
+      ],
+
+      mannerHeading: "We offer services in the following manner",
+      manner: [
+        "Business Management",
+        "Organizational Behavior",
+        "Operations Management",
+        "Change Management",
+        "Risk Management",
+        "Supply Chain Management",
+        "Human Resource Management",
+        "Project Management",
+        "Strategic Management",
+      ],
+    },
+    {
+      title: "Accounting",
+      logo: "logo",
+      about: `Accounting, also known as accountancy, is the process of recording and processing information about economic entities, such as businesses and corporations. Accounting measures the results of an organization's economic activities and conveys this information to a variety of stakeholders, including investors, creditors, management, and regulators. Practitioners of accounting are known as accountants. The terms "accounting" and "financial reporting" are often used interchangeably.`,
+      service: "Services Offered",
+      points: [
+        "Accounting assignment help",
+        "Doing an Accounting assignment",
+        "Offering pre-made Accounting assignments for sale",
+      ],
+      mannerHeading: "We offer services in the following manner",
+      manner: [
+        "Auditing",
+        "Finance",
+        "Taxation Accounting",
+        "MYOB",
+        "Corporate Accounting",
+        "Management Accounting",
+        "Cost Accounting",
+        "Accounting Information Systems",
+        "International Accounting",
+        "Accounting Society",
+      ],
+    },
+    {
+      title: "Engineering Studies",
+      logo: "logo",
+      about: "Engineering studies is an interdisciplinary branch of social sciences and humanities devoted to the study of engineers and their activities, often considered a part of science and technology studies (STS), and intersecting with and drawing from engineering education research. Studying engineers refers among other to the history and the sociology of their profession, its institutionalization and organization, the social composition and structure of the population of engineers, their training, their trajectory, etc. A subfield is for instance Women in engineering. Studying engineering refers to the study of engineering activities and practices, their knowledge and ontologies, their role into the society, their engagement.",
+      service: "Services Offered",
+      points: ["Engineering assignment help", "Doing an Engineering assignment","Offering pre-made Engineering assignments for sale"],
+      mannerHeading: "We offer services in the following manner",
+      manner: ["Mechanical Engineering","Civil Engineering", "Software Engineering", "Chemical Engineering","Biomedical Engineering","Electronic Engineering","Aerospace Engineering","Computer Engineering","Electrical Engineering","Engineering Mathematics"     ],
+    },
+    {
+      title: "Information Technology",
+      logo: "logo",
+      about: "Information technology (IT) is a set of related fields that encompass computer systems, software, programming languages and data and information processing and storage. IT forms part of information and communications technology (ICT). An information technology system (IT system) is generally an information system, a communications system, or, more specifically speaking, a computer system — including all hardware, software, and peripheral equipment — operated by a limited group of IT users, and an IT project usually refers to the commissioning and implementation of an IT system.",
+      service: "Services Offered",
+      points: ["IT assignment help", "Doing an IT assignment", "Offering pre-made IT assignments for sale"    ],
+      mannerHeading: "We offer services in the following manner",
+      manner: ["Networking","Programming","R Programming","C Programming","Linear Programming","Java","JavaScript Assignment","Python Assignment","Ruby Assignment","IT Security"      ],
+    },
+    {
+      title: "Law",
+      logo: "logo",
+      about: "Legal education is the education of individuals in the principles, practices, and theory of law. It may be undertaken for several reasons, including to provide the knowledge and skills necessary for admission to legal practice in a particular jurisdiction, to provide a greater breadth of knowledge to those working in other professions such as politics or business, to provide current lawyers with advanced training or greater specialization, or to update lawyers on recent developments in the law.",
+      service: "Services Offered",
+      points: ["Law assignment help","Doing a law assignment","Offering pre-made law assignments for sale"    ],
+      mannerHeading: "We offer services in the following manner",
+      manner: ["Taxation Law", "Commercial Law",
+      "Business Law",
+      "Criminal Law",
+      "Employment Law",
+      "Constitutional Law",
+      "Civil Law",
+      "Property Law",
+      "Corporate Law",
+      "Tort Law"     ],
+    },
+    {
+      title: "Statistics",
+      logo: "logo",
+      about: `Statistics (from German: Statistik, orig. "description of a state, a country") is the discipline that concerns the collection, organization, analysis, interpretation, and presentation of data. In applying statistics to a scientific, industrial, or social problem, it is conventional to begin with a statistical population or a statistical model to be studied. Populations can be diverse groups of people or objects such as "all people living in a country" or "every atom composing a crystal". Statistics deals with every aspect of data, including the planning of data collection in terms of the design of surveys and experiments.`,
+      service: "Services Offered",
+      points: ["Statistics assignment help", "Doing a Statistics assignment","Offering pre-made Statistics assignments for sale"  ],
+      mannerHeading: "We offer services in the following manner",
+      manner: [
+        "Statistics",
+        "STATA",
+        "Econometrics",
+        "R Studio",
+        "Data Analysis",
+        "Data Mining",
+        "Business Statistics",
+        "Financial Statistics",
+        "Descriptive Statistics",
+        "Statistics Management Accounting"
+      ],
+    },
+    {
+      title: "General Subjects",
+      logo: "logo",
+      about: "We offer services for a variety of academic stages from the basic topics in schools to dissertations of exceptional merit. Here are some non-branch specific subjects that might be of some help:",
+      aboutPoints:[
+        "Political Science",
+        "Social Science",
+        "Geography",
+        "Psychology",
+        "Education Plan",
+        "Humanities",
+        "Anthropology",
+        "Philosophy",
+        "Physics",
+        "Probability",
+
+      ],
+      service: "",
+      points: [],
+      mannerHeading: "",
+      manner: [],
+    },
+  ];
+  return <div className=""></div>;
+}
+
+export default Subject;
